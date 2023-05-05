d9a57882d6e02493d8798a040693806982c21ef1
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-05-05 16:12:51 +0200

    add function

--- index.php
+++ index.php
@@ -15,7 +15,25 @@ Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, l
 In allegato uno screenshot qualora non aveste l'ispirazione creativa! :arte:
 Confermate lettura come al solito e buon weekend! -->
 <?php
-
+$passwordlenght = isset($_GET['password_lenght']) ? $_GET['password_lenght'] : 0;
+$passwordlenght = is_numeric($_GET['password_lenght']) ? $_GET['password_lenght'] : 0 ;
+echo (isset($_GET['password_lenght']));
+echo (is_numeric($passwordlenght) );
+echo($passwordlenght);
+$chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" ;
+function generator_random_passwor ($passwordlenght, string $chars){
+    $var_size = strlen($chars) - 1;
+    for( $x = 0; $x < $passwordlenght; $x++ ) {
+        $random_str = '';
+        if (stripos($random_str , $chars[ rand( 0, $var_size) ])) {
+            $random_str = $random_str . $chars[ rand( 0, $var_size) ];
+            echo $random_str;  
+            return $random_str;
+        };
+    };
+}
+$pass = generator_random_passwor($passwordlenght,$chars);
+echo $pass;
 ?>
 <!doctype html>
 <html lang="en">
@@ -27,63 +45,70 @@ Confermate lettura come al solito e buon weekend! -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
 </head>
 
 <body>
-    <header>
-        <h1><strong>Srong Password Generator</strong></h1>
-        <h2>Genera una password sicura</h2>
+    <header class="bg-dark p-5">
+        <h1 class="text-center text-black-50"><strong>Srong Password Generator</strong></h1>
+        <h2 class="text-center text-white my-0">Genera una password sicura</h2>
     </header>
-    <main>
-        <div class="container">
-            <div class="row ">
-                <div class="col">
-                    <div class="">
-                        <p>Nesun parametro valido inserito</p>
+    <main class=" bg-dark p-5">
+        <div class="container p-3 my-0">
+            <div class="row flex-column align-items-center">
+                <div class="col-8 card m-4 bg-light">
+                    <div class="alert-success ">
+                        <p class="text-success">Nesun parametro valido inserito</p>
                     </div>
                 </div>
-                <div class="col">
-                    <form action="">
-                        <div>
-                            <p>Lunghezza password:</p>
-                            <p>Consenti ripetizioni di uno o più caratteri:</p>
+                <form action="" method="get" class="col-8 card  d-flex justify-content-center p-3">
+                        <div class="col d-flex ">
+                            <div class="px-5">
+                                <p>Lunghezza password:</p>
+                                <p>Consenti ripetizioni di uno o più caratteri:</p>
+                            </div>
+                            <div class="px-5">
+                                <!-- Lunghezza password -->
+                                <div>
+                                    <label for="password_lenght"></label>
+                                    <input type="text" name="password_lenght" id="password_lenght">
+                                </div>
+
+                               <!--  <div>
+                                    <input type="radio" name="true" id="true" value="true">
+                                    <label for="true">Si</label>
+                                </div>
+
+                                <div>
+                                    <input type="radio" name="false" id="false" value="false">
+                                    <label for="false">No</label>
+                                </div>
+
+                                <div>
+                                    <input type="checkbox" name="words" id="words">
+                                    <label for="words">Lettere</label>
+                                </div>
+
+                                <div>
+                                    <input type="checkbox" name="numbers" id="numbers">
+                                    <label for="numbers">Numeri</label>
+                                </div>
+
+                                <div>
+                                    <input type="checkbox" name="simbols" id="simbols">
+                                    <label for="simbols">Simboli</label>
+                                </div> -->
+                            </div>
                         </div>
-                        <div>
-
-                            <div>
-                                <label for="password_lenght"></label>
-                                <input type="text" name="password_lenght" id="password_lenght">
-                            </div>
-
-                            <div>
-                                <input type="radio" name="true" id="true" value="true">
-                                <label for="true">Si</label>
-                            </div>
-
-                            <div>
-                                <input type="radio" name="false" id="false" value="false">
-                                <label for="false">No</label>
-                            </div>
-
-                            <div>
-                                <input type="checkbox" name="words" id="words">
-                                <label for="words">Lettere</label>
-                            </div>
-
-                            <div>
-                                <input type="checkbox" name="numbers" id="numbers">
-                                <label for="numbers">Numeri</label>
-                            </div>
-
-                            <div>
-                                <input type="checkbox" name="simbols" id="simbols">
-                                <label for="simbols">Simboli</label>
-                            </div>
+                        <div class="col px-5">
+                            <button type="submit" class="btn btn-primary">invia</button>
+                            <button type="reset" class="btn btn-light">Annulla</button>
                         </div>
-                    </form>
-                </div>
+
+                    
+                </form>
             </div>
         </div>
     </main>
@@ -91,7 +116,9 @@ Confermate lettura come al solito e buon weekend! -->
         <!-- place footer here -->
     </footer>
     <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
 
 </body>
 
